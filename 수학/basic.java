a1fd55436e8617fa3cf8fb71f86d9ba98494b04a
Seongbae103 <seongbae103@gmail.com>
2022-10-12 19:26:20 +0900

    10-12

---
+++ 수학/basic.java
@@ -0,0 +1,13 @@
+import javax.swing.event.SwingPropertyChangeSupport;
+
+class basic {
+    public static void main(String[] args) {
+        basic solution = new basic();
+        System.out.println(solution.solution());
+    }
+    String solution(){
+        Stirng title = "###";
+        return title;
+    }
+
+}
