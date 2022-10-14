c3c5a997e2bd6859a4745fd7e7c7eae33b9e6d1a
Seongbae103 <seongbae103@gmail.com>
2022-10-14 17:59:27 +0900

    10-13 17:59 버블정렬

---
+++ 정렬/버블.java
@@ -0,0 +1,44 @@
+package 정렬;
+import java.util.*;
+class 버블정렬 {
+    public static void main(String[] args) {
+        new 버블정렬().solution();
+    }
+    void solution(){
+        int[] arr = createArray();
+        arr = sortArray(arr);
+        printArray(arr); 
+    }
+    int createRandomNumber(){
+        return (int)(Math.random()*100)+1;
+    }
+    int[] createArray(){
+        int[] arr = new int[10];
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = createRandomNumber();
+            for(int j = 0; j<i; j++){
+              if(arr[i] == arr[j]){i--;}
+            }
+        }
+        return arr; 
+    }
+    int[] sortArray(int[] arr){
+        for(int i =0; i<arr.length; i++){
+            for(int j = 0; j<arr.length-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = t;
+                }    
+            }
+        } 
+        return arr;        
+    }
+    void printArray(int[] arr){
+        for(int i =0; i<arr.length; ++i){
+        System.out.println(arr[i]+"\t");;
+         }
+    }
+}
+
+
---
+++ 정렬/삽입.java
@@ -0,0 +1,36 @@
+package 정렬;
+import java.util.*;
+
+class 땡땡이정렬 {
+    public static void main(String[] args) {
+        new 땡땡이정렬().solution();
+
+    }
+    void solution(){
+        int[] arr = a();
+        arr = b(arr);
+        arr = c(arr);
+        arr = d(arr);
+        arr = e(arr);
+        f(arr);
+    }
+    int[] a(){
+        int[] arr = new int[5];
+        return arr;
+    }
+    int[] b(int[] arr){
+        return arr;
+    }
+    int[] c(int[] arr){
+        return arr;
+    }
+    int[] d(int[] arr){
+        return arr;
+    }
+    int[] e(int[] arr){
+        return arr;
+    }
+    void f(int[] arr){
+
+    }
+}
